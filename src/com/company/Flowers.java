1159fee23eea7bc4ed1584011e5840c3ec541ee5
Marria96 <mashagirnyak@gmail.com>
2022-05-27 17:44:08 +0300

    Initial commit

---
+++ src/com/company/Flowers.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Flowers {
+    private String name;
+    private int price;
+
+    public Flowers(String color, int price) {
+        this.name = color;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
---
+++ src/com/company/Rose.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Rose extends Flowers{
+    public Rose(String color, int price) {
+        super(color, price);
+    }
+
+}
